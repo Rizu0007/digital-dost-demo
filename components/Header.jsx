61e947cfc6f7d4b0a422ad0be233cb3155f76f2d
AbdullahShahid5112 <abdullahshahid5112@gmail.com>
2024-11-15 13:02:24 +0500

    header blue rotate image size increase

--- components/Header.jsx
+++ components/Header.jsx
@@ -6,93 +6,114 @@ import Link from 'next/link'
 
 function Header() {
   return (
-    <div className='flex xs:flex-col md:flex-row bg-white bg-custom-image1 bg-no-repeat bg-right-top pt-5 pb-40'>
+    <>
+      <div className='flex xs:flex-col md:flex-row bg-white bg-custom-image1 bg-no-repeat bg-right-top pt-5 pb-60'>
 
-      <div className='md:w-[60%] xs:pb-20 xs:pt-10 sm:py-20 xs:pl-5 md:pl-20 space-y-16 py-10 mt-10 mx-5'>
-        <div className='flex items-center justify-between'>
-          <p className="text-black font-bold leading-[80px] xs:w-[350px] sm:w-full xs:text-3xl sm:text-4xl md:text-5xl lg:text-6xl xl:text-7xl">
-            Digital Product Development Company
+        <div className='xs:w-full md:w-[60%] xs:pb-20 xs:pt-10 sm:py-20 xs:pl-5 md:pl-20 space-y-16 py-10 mt-10 mx-5'>
+          <div className='flex items-center justify-between'>
+            <p className="text-black font-bold leading-[80px]  w-full xs:text-3xl sm:text-4xl md:text-5xl lg:text-6xl xl:text-7xl">
+              Digital Product Development Company
+            </p>
+
+            {/* Rotating Image for Mobile */}
+            <Link href='/calendly'>
+              <div className="relative flex justify-center items-center w-[170px] h-[170px] hover-pop-out mr-5 md:hidden">
+                {/* Outer Circular Text Image with Spinning Animation */}
+                <div className="absolute inset-0 flex justify-center items-center animate-spin-slow pop-out">
+                  <Image
+                    src="/Header/rotte.svg"
+                    alt="Circular Text"
+                    className="w-full h-full object-contain hidden lg:block"
+                    width={50}
+                    height={50}
+                  />
+                  <Image
+                    src="/Header/rotatesmall.svg"
+                    alt="Circular Text"
+                    className="w-full h-full object-contain lg:hidden"
+                    width={50}
+                    height={50}
+                  />
+                </div>
+
+                {/* Blue Circle with Arrow */}
+                <div className="bg-[#00729f] h-[75px] w-[75px] rounded-full flex justify-center items-center shadow-lg z-10 pop-out-fast">
+                  <Image
+                    src="/Header/arrowup.svg"
+                    alt="Arrow Up"
+                    className="w-5 h-5" // Adjusted size for the arrow icon
+                    width={70}
+                    height={70}
+                  />
+                </div>
+              </div>
+            </Link>
+          </div>
+
+          <p className='h-1 text-[#333333] text-2xl font-extralight mr-7'>
+            We handle each stage of your digital product’s journey, from initial discovery, growth design and development to AI implementation, coupled with post-launch support and maintenance.
           </p>
 
-          {/* Rotating Image for Mobile */}
+        </div>
+
+
+
+        <div className="hidden md:flex justify-end items-end md:w-[40%]">
           <Link href='/calendly'>
-          <div className="relative flex justify-center items-center w-[100px] h-[100px] hover-pop-out mr-5 md:hidden">
-            {/* Outer Circular Text Image with Spinning Animation */}
-            <div className="absolute inset-0 flex justify-center items-center animate-spin-slow pop-out">
-              <Image
-                src="/Header/rotte.svg"
-                alt="Circular Text"
-                className="w-full h-full object-contain hidden lg:block"
-                width={50}
-                height={50}
-              />
-              <Image
-                src="/Header/rotatesmall.svg"
-                alt="Circular Text"
-                className="w-full h-full object-contain lg:hidden"
-                width={50}
-                height={50}
-              />
-            </div>
+            <div className="relative flex justify-center items-center w-[200px] h-[200px] hover-pop-out mr-10 ">
+              {/* Outer Circular Text Image with Spinning Animation */}
+              <div className="absolute inset-0 flex justify-center items-center animate-spin-slow pop-out">
+                <Image
+                  src="/Header/rotte.svg"
+                  alt="Circular Text"
+                  className="w-full h-full object-contain hidden lg:block"
+                  width={50}
+                  height={50}
+                />
+                <Image
+                  src="/Header/rotatesmall.svg"
+                  alt="Circular Text"
+                  className="w-full h-full object-contain lg:hidden"
+                  width={50}
+                  height={50}
+                />
+              </div>
 
-            {/* Blue Circle with Arrow */}
-            <div className="bg-[#00729f] h-[30px] w-[30px] rounded-full flex justify-center items-center shadow-lg z-10 pop-out-fast">
-              <Image
-                src="/Header/arrowup.svg"
-                alt="Arrow Up"
-                className="w-3 h-3" // Adjusted size for the arrow icon
-                width={50}
-                height={50}
-              />
+              {/* Blue Circle with Arrow */}
+              <div className="bg-[#00729f] h-[120px] w-[120px] rounded-full flex justify-center items-center shadow-lg z-10 pop-out-fast sm:h-[100px] sm:w-[100px] xs:h-[80px] xs:w-[80px]">
+                <Image
+                  src="/Header/arrowup.svg"
+                  alt="Arrow Up"
+                  className="w-8 h-8"
+                  width={50}
+                  height={50}
+                />
+              </div>
             </div>
-          </div>
           </Link>
+
         </div>
 
-        <p className='h-1 text-[#333333] text-2xl font-extralight'>
-          We handle each stage of your digital product’s journey, from initial discovery, growth design and development to AI implementation, coupled with post-launch support and maintenance.
-        </p>
+
       </div>
 
 
-    
-      <div className="hidden md:flex justify-end items-end md:w-[40%]">
-      <Link href='/calendly'>
-        <div className="relative flex justify-center items-center w-[200px] h-[200px] hover-pop-out mr-10 ">
-          {/* Outer Circular Text Image with Spinning Animation */}
-          <div className="absolute inset-0 flex justify-center items-center animate-spin-slow pop-out">
-            <Image
-              src="/Header/rotte.svg"
-              alt="Circular Text"
-              className="w-full h-full object-contain hidden lg:block"
-              width={50}
-              height={50}
-            />
-            <Image
-              src="/Header/rotatesmall.svg"
-              alt="Circular Text"
-              className="w-full h-full object-contain lg:hidden"
-              width={50}
-              height={50}
-            />
-          </div>
-
-          {/* Blue Circle with Arrow */}
-          <div className="bg-[#00729f] h-[120px] w-[120px] rounded-full flex justify-center items-center shadow-lg z-10 pop-out-fast sm:h-[100px] sm:w-[100px] xs:h-[80px] xs:w-[80px]">
-            <Image
-              src="/Header/arrowup.svg"
-              alt="Arrow Up"
-              className="w-8 h-8"
-              width={50}
-              height={50}
-            />
-          </div>
+      <div className="xs:-mt-24 sm:-mt-28 lg:-mt-40 text-white p-4 w-full flex justify-start md:ml-20 ">
+        <div className="flex w-full md:w-1/2">
+          <input
+            type="text"
+            placeholder="Enter your website"
+            className="w-full bg-white text-gray-700 border border-gray-300 rounded-l-md py-2 px-4"
+          />
+          <button
+            className="bg-[#00729f] hover:bg-[#3d8dad] text-white font-bold py-2 px-4 rounded-r-md"
+          >
+            Send a proposal
+          </button>
         </div>
-        </Link>
       </div>
+    </>
 
-
-    </div>
   )
 }
 
