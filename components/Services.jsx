4e15bfd8f2fb3980b4d1fcdae72c3a5b3e4353c4
AbdullahShahid5112 <AbdullahShahid5112@gmail.com>
2024-11-20 15:57:19 +0500

    fixed responsiveness

--- components/Services.jsx
+++ components/Services.jsx
@@ -68,7 +68,7 @@ const Service = [
 ];
 const CustomPrevArrow = ({ onClick }) => (
   <div
-    className="bg-[#fff3] border-solid border-2 border-[#00729F] rounded-full h-11 w-11 flex justify-center items-center absolute left-[-55px] top-[50%] transform -translate-y-1/2 z-10"
+    className="bg-[#fff3] border-solid border-2 border-[#00729F] rounded-full h-11 w-11 flex justify-center items-center absolute xs:left-1/3 xs:transform xs:-translate-x-1/2 xs:top-[110%] sm:left-[-35px] sm:top-[50%] sm:transform sm:-translate-y-1/2 z-10"
     onClick={onClick}
   >
     <Image
@@ -82,8 +82,8 @@ const CustomPrevArrow = ({ onClick }) => (
 
 const CustomNextArrow = ({ onClick }) => (
   <div
-    className="bg-[#fff3] border-solid border-2 border-[#00729F] rounded-full h-11 w-11 flex justify-center items-center absolute right-[-40px] top-[50%] transform -translate-y-1/2 z-10"
-    onClick={onClick}
+  className="bg-[#fff3] border-solid border-2 border-[#00729F] rounded-full h-11 w-11 flex justify-center items-center absolute xs:right-1/3 xs:transform xs:translate-x-1/2 xs:top-[110%] sm:right-[-35px] sm:top-[50%] sm:transform sm:-translate-y-1/2 z-10"
+  onClick={onClick}
   >
     <Image
       src="/Podcast/whitearrow.svg"
@@ -93,6 +93,7 @@ const CustomNextArrow = ({ onClick }) => (
     />
   </div>
 );
+
 
 export default function ServicesComponent() {
   // State to hold the currently selected service and card index
@@ -126,14 +127,14 @@ export default function ServicesComponent() {
           slidesToShow: 3,
           slidesToScroll: 3,
           infinite: true,
-          dots: true
+          dots: false
         }
       },
       {
-        breakpoint: 600,
+        breakpoint: 768,
         settings: {
-          slidesToShow: 2,
-          slidesToScroll: 2,
+          slidesToShow: 1.5,
+          slidesToScroll: 1,
           initialSlide: 2
         }
       },
@@ -149,21 +150,22 @@ export default function ServicesComponent() {
    
   }
   return (
-    <div className="mt-20 md:px-[60px] lg:px-[100px] xl:px-[124.5px] pt-[274px] pb-[230px]">
+    <div className="mt-20 md:px-[60px] lg:px-[100px] xl:px-[124.5px] xs:pt-[150px] pt-[274px] pb-[230px] xs:px-4">
       {/* Main Service Display */}
-      <div className="w-full flex justify-between h-[500px]">
+      <div className="w-full flex xs:flex-col-reverse lg:flex-row justify-between h-[500px]">
         {/* Main Image */}
-        <div>
+        <div className="xs:flex xs:justify-center" >
           <Image
             src={selectedService.imagesrc}
             alt={selectedService.title}
             height={517}
             width={625}
+            className=" xs:w-[500px] xs:h-[350px] md:h-[517px] md:w-[625px]"
           />
         </div>
 
         {/* Main Service Info */}
-        <div className="flex flex-col justify-center items-center w-[40%]">
+        <div className="flex  justify-center items-center xs:w-full lg:w-[40%]">
           <div>
             <p className="text-[14px] text-[#171717CC] font-semibold">
               Our Services
@@ -172,8 +174,13 @@ export default function ServicesComponent() {
             <p className="text-[#171717CC] xs:text-[14px]md:text-xl mt-5 mb-10">
               {selectedService.description}
             </p>
+
+
+            <div className="flex justify-between">
+
+            
             <Link href='/services' >
-              <button className="bg-[#00729f] xl:h-[45px] xl:w-40 lg:h-[45px] lg:w-[144px] rounded-lg text-white shadow-lg font-bold text-[16px] w-full h-11 ml-5">
+              <button className="bg-[#00729f]  xs:w-[150px] sm:w-[300px] h-14 xl:h-[45px] xl:w-40 lg:h-[45px] lg:w-[144px] rounded-lg text-white shadow-lg font-bold text-[16px] ">
                 <div className="flex justify-center">
                   <div >
                     <p>VIEW MORE</p>
@@ -189,7 +196,7 @@ export default function ServicesComponent() {
               </button>
             </Link>
             <Link href='/calendly' >
-              <button className="bg-[#00729f] xl:h-[45px] xl:w-40 lg:h-[45px] lg:w-[144px] rounded-lg text-white shadow-lg font-bold text-[16px] w-full h-11 mt-5 ml-5">
+              <button className="bg-[#00729f] xl:h-[45px] xl:w-40 lg:h-[45px] lg:w-[144px] rounded-lg text-white shadow-lg font-bold text-[16px] xs:w-[150px] sm:w-[300px] h-14 xs:ml-2">
                 <div className="flex justify-center">
                   <div >
                     <p>CONTACT US</p>
@@ -204,12 +211,13 @@ export default function ServicesComponent() {
                 </div>
               </button>
             </Link>
+            </div>
           </div>
         </div>
       </div>
 
       {/* Service Cards */}
-      <div className="flex mt-10 justify-center xs:hidden lg:block">
+      <div className="flex mt-10 justify-center ">
         <Slider
           {...settings}
           className="w-[80vw] lg:min-w-[700px] xl:min-w-[1150px] 2xl:max-w-[1300px]  " // Fixed the class syntax
@@ -217,7 +225,7 @@ export default function ServicesComponent() {
           {Service.map((srvc, index) => (
             <div key={index} className="">
               <div
-                className={`pt-[38px] pr-[30px] pb-[24px] pl-[28px] lg:w-11/12 lg:h-52 xl:w-11/12 xl:h-52 2xl:w-72 2xl:h-52 rounded-xl shadow-xl group cursor-pointer transition-all duration-300 ${selectedCardIndex === index ? "bg-box grow h-60 text-white" : "bg-[#ffffff]"
+                className={`pt-[38px] pr-[30px] pb-[24px] pl-[28px]  w-11/12 h-52 xl:w-11/12 xl:h-52 2xl:w-72 2xl:h-52 rounded-xl shadow-xl group cursor-pointer transition-all duration-300 ${selectedCardIndex === index ? "bg-box grow h-60 text-white" : "bg-[#ffffff]"
                   }`}
                 onClick={() => {
                   setSelectedService(srvc);
